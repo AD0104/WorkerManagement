a7e260c990e808256124696a8392a57071eb4cbd
Adrian Hernandez <ad24.ra12@gmail.com>
2023-02-18 01:28:29 -0600

    1.-Added navbar to base.html, now all templates will have the navbar. 2.-Added new dinamyc route to load users. 3.-Modified main.html, each card (with the button for 'Ver Mas') shows more info 4.-Done design (and markup) for employee page

--- app/db_services.py
+++ app/db_services.py
@@ -2,10 +2,11 @@ from flask_mysqldb import MySQL
 
 conn = MySQL()
 
-workers_fields_titles = ["name","last_name","age","sex","birth_date","curp","entry_date",
+workers_field_titles_full = ["name","last_name","age","sex","birth_date","curp","entry_date",
                         "position","branch","minutely_salary","hourly_salary","daily_salary",
                         "biweekly_salary","monthly_salary","vacation_assigned_days","vacation_taken_days",
                         "vacation_remaining_days"]
+workers_field_titles_less = ["id","name","position","daily_salary","biweekly_salary","monthly_salary"]
 
 def get_user(usr_name: str) -> dict:
     crs = conn.connection.cursor()
@@ -14,7 +15,37 @@ def get_user(usr_name: str) -> dict:
     if result:
         return {"usr-name": result[0], "usr-passwrd":result[1]}
     return {} 
-def get_workers()->dict:
+def get_workers_resumed_data()->dict:
+    crs = conn.connection.cursor()
+    crs.execute(""" SELECT id,name, position, daily_salary, biweekly_salary, monthly_salary
+                FROM TB_Employees""")
+    query_result = crs.fetchall()
+    return_dict = {}
+    row_count = 1
+    for row in query_result:
+        tmp_dict = {}
+        for key,value in zip(workers_field_titles_less, row):
+            tmp_dict[key] = value
+        return_dict[f"worker_{row_count}"] = tmp_dict
+        row_count+=1
+    return return_dict
+
+def get_single_worker_data(id)->dict:
+    crs = conn.connection.cursor()
+    crs.execute(f"""SELECT name,last_name,age,sex,birth_date,curp,entry_date,
+                        position,branch,minutely_salary,hourly_salary,daily_salary,
+                        biweekly_salary,monthly_salary,vacation_assigned_days,vacation_taken_days,
+                        vacation_remaining_days 
+                FROM TB_Employees
+                WHERE id={id}""")
+    query_result = crs.fetchall()
+    return_dict = {}
+    for row in query_result:
+        for key, value in zip(workers_field_titles_full, row):
+            return_dict[key] = str(value)
+    return return_dict
+
+def get_workers_full_data()->dict:
     crs = conn.connection.cursor()
     crs.execute("""SELECT name,last_name,age,sex,birth_date,curp,entry_date,
                         position,branch,minutely_salary,hourly_salary,daily_salary,
@@ -26,7 +57,7 @@ def get_workers()->dict:
     row_count = 1
     for row in result:
         tmp_dict = {}
-        for key, value in zip(workers_fields_titles, row):
+        for key, value in zip(workers_field_titles_full, row):
             tmp_dict[key] = str(value)
         return_dict[f"worker_{row_count}"] = tmp_dict
         row_count+=1
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from app import create_app
-from flask import render_template
+from flask import redirect, render_template, url_for
 from flask_login import login_required
 
-from app.db_services import get_workers
+from app.db_services import get_workers_resumed_data, get_single_worker_data
 
 app = create_app()
 
@@ -14,6 +14,15 @@ def index():
 @login_required 
 def main_menu():
     context = {
-        "worker_info": get_workers()
+        "worker_info": get_workers_resumed_data()
             }
     return render_template('main.html', **context)
+
+@app.route("/menu/employee/<int:id>")
+@login_required
+def single_employee_info(id):
+    context = {
+        "worker_info": get_single_worker_data(id)
+    }
+    return render_template("employee.html", **context)
+    
